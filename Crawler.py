a30ec12e87b4b85dea323d75516f78a4a39a9bc9
Riceair <handy00095@gmail.com>
2023-09-20 15:41:38 +0800

    name and price

--- Crawler.py
+++ Crawler.py
@@ -1,24 +1,33 @@
+from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
 
 class WebCrawler:
-    def crawl_one_page(self, url):
+    def __init__(self):
+        self.products = []
+
+    def request_one_page(self, url):
         req = requests.get(url)
-        names, prices = self.getNamePrice(req)
+        self.recordNamePrice(req)
     
-    def getNamePrice(self, req):
+    def recordNamePrice(self, req):
         soup = BeautifulSoup(req.text, "html.parser")
-        names = soup.find_all("p", attrs={"class":"D_pt M_kA D_oq M_jR D_pu M_kB D_py M_kG D_p_ M_kI D_pE M_kM D_pH M_kP D_pK"})
-        prices = soup.find_all("p", attrs={"class":"D_pt M_kA D_oq M_jR D_pu M_kB D_py M_kG D_p_ M_kI D_pE M_kM D_pG M_kO D_pJ"})
-        names = [str(name) for name in names] # convert to string
-        prices = [str(price) for price in prices] # convert to string
-        # Name example
-        # <p class="D_pt M_kA D_oq M_jR D_pu M_kB D_py M_kG D_p_ M_kI D_pE M_kM D_pH M_kP D_pK">LOVFEE 圓領坑條針織上衣 無袖 背心</p>
-        names = [name[name.index(">")+1:name.index("</")] for name in names]
-        prices = [price[price.index(">")+1:price.index("</")] for price in prices]
-        return names, prices
+        blocks = soup.find_all("div", attrs={"class":"D__N D__v"}) # get product-blocks
+        for block in blocks:
+            block = str(block)
+            p_starts = self.findAllSubStr(block, "<p")
+            p_ends = self.findAllSubStr(block, "</p>")
+            name = self.getPText(block[p_starts[2]:p_ends[2]])
+            price = self.getPText(block[p_starts[3]:p_ends[3]])
+            self.products.append((name, price))
+
+    def findAllSubStr(self, string, target):
+        return [i for i in range(len(string)) if string.startswith(target, i)]
+    
+    def getPText(self, substring):
+        return substring[substring.index(">")+1:]
 
 if __name__=="__main__":
     url = "https://tw.carousell.com/categories/women-s-fashion-4/"
     crawler = WebCrawler()
-    crawler.crawl_one_page(url)
+    # crawler.request_one_page(url)
